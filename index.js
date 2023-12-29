39e54948d29182fa9128233810273f68b1102f80
Joseph Garcia <prosif@gmail.com>
2023-12-28 18:25:18 -0700

    more

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class DoDad extends Game {
             coordinates2d: ShapeUtils.rectangle(0, 0, 0, 0)
         });
 
-        console.log('change 3');
+        console.log('change 4');
 
         const layerBaseNode = layerBase(this.handleLayerClick);
         this.base.addChild(layerBaseNode);
